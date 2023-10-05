37bbae9d50e7ab86362aac8b12583fb88bd9197a
dci-student <glaventzakis@gmail.com>
2023-10-05 14:35:39 +0200

    new text file

--- Creator.java
+++ Creator.java
@@ -1,2 +1,62 @@
+import java.util.Random;
+
 public class Creator {
+    private String name;
+    private String role;
+    private int health;
+    private int strength;
+    private int magic;
+
+    public Creator(String name) {
+        this.name = name;
+        this.role = generateRandomRole();
+        this.health = generateRandomStat();
+        this.strength = generateRandomStat();
+        this.magic = generateRandomStat();
+
+        // Adjust stats based on the role
+        if (role.equals("Warrior")) {
+            this.strength *= 3;
+        } else if (role.equals("Wizard")) {
+            this.magic *= 3;
+        } else if (role.equals("Potato")) {
+            this.health *= 3;
+        }
+    }
+
+    private String generateRandomRole() {
+        String[] roles = {"Warrior", "Wizard", "Potato"};
+        Random random = new Random();
+        return roles[random.nextInt(roles.length)];
+    }
+
+    private int generateRandomStat() {
+        Random random = new Random();
+        return random.nextInt(11) + 2;  // Random number between 2 and 12
+    }
+
+    @Override
+    public String toString() {
+        return "Character{" +
+                "name='" + name + '\'' +
+                ", role='" + role + '\'' +
+                ", health=" + health +
+                ", strength=" + strength +
+                ", magic=" + magic +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java Creator <character_name>");
+            return;
+        }
+
+        String characterName = args[0];
+        Creator character = new Creator(characterName);
+
+        // Display character information
+        System.out.println("Generated character:");
+        System.out.println(character);
+    }
 }
